b984174f7c592e7614a07ef819784eaa20b43b66
Omkar Parab <omcar04@gmail.com>
2020-06-18 21:10:08 +0530

    Updates

---
+++ precache-manifest.b9fc256dc670ad7d34ff0fb860f71a0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6080c63ca20cf5d54843af9ab3705634",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "fef12a2e36d4b653b341",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0e28cacc76671f98eaa3",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "fef12a2e36d4b653b341",
+    "url": "/contactmanager/static/js/2.0119f296.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.0119f296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e28cacc76671f98eaa3",
+    "url": "/contactmanager/static/js/main.1a2944f4.chunk.js"
+  },
+  {
+    "revision": "2e3ca3753f4d9c27bac8",
+    "url": "/contactmanager/static/js/runtime-main.354e0907.js"
+  }
+]);
