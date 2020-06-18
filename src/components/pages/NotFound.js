fcde20b88d54098a760929ac15e7e87ce29958b6
Omkar Parab <omcar04@gmail.com>
2020-06-18 21:03:44 +0530

    Initial commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NotFind() {
+  return (
+    <div>
+      <h1 className="display-4">
+        {" "}
+        <span className="text-danger">404</span> Page Not Found
+      </h1>
+    </div>
+  );
+}
